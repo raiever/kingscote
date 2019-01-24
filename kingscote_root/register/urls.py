b7b19377bccad3319d0d269b2aeca57e771c1f35
SungHwan <ray.sunghwan@gmail.com>
2019-01-24 10:31:18 +0000

    24_Jan_2019
    Ch12 is completed.

--- kingscote_root/register/urls.py
+++ kingscote_root/register/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from . import views
-from .views import StudentList
+from .views import StudentList, StudentView
 
 urlpatterns = [
     path('', views.student_reg, name='student-register-request'),
+    path('show/<int:pk>', StudentView.as_view(), name='student-detail'),
     path('show', StudentList.as_view(), name='show-quotes'),
 ]
--- kingscote_root/register/views.py
+++ kingscote_root/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Student
 from .forms import StudentForm
@@ -9,6 +10,20 @@ from pages.models import Page
 class StudentList(ListView):
     model = Student
     context_object_name = 'all_students'
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
+class StudentView(DetailView):
+    model = Student
+    context_object_name = 'student_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
 
 def student_reg(request):
     submitted = False

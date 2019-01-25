ba376b13f03d65c5c646ca3ab7d6659495b38955
SungHwan <ray.sunghwan@gmail.com>
2019-01-25 11:37:01 +0000

    25_Jan_2019
    Ch13 completed

--- kingscote_root/register/views.py
+++ kingscote_root/register/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
@@ -7,8 +13,9 @@ from .models import Student
 from .forms import StudentForm
 from pages.models import Page
 
-class StudentList(ListView):
-    model = Student
+class StudentList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
+    # model = Student
     context_object_name = 'all_students'
 
     def get_context_data(self, **kwargs):
@@ -16,21 +23,46 @@ class StudentList(ListView):
         context['page_list'] = Page.objects.all()
         return context
 
-class StudentView(DetailView):
-    model = Student
+    def get_queryset(self):
+        return Student.objects.filter(username=self.request.user)
+
+
+class StudentView(LoginRequiredMixin, DetailView):
+    login_url = reverse_lazy('login')
+    # model = Student
     context_object_name = 'student_detail'
 
     def get_context_data(self, **kwargs):
         context = super(StudentView, self).get_context_data(**kwargs)
         context['page_list'] = Page.objects.all()
         return context
+    
+    def get_queryset(self):
+        return Student.objects.filter(username=self.request.user)
 
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
+@login_required(login_url=reverse_lazy('login'))
 def student_reg(request):
     submitted = False
     if request.method == 'POST':
         form = StudentForm(request.POST, request.FILES)
+        
         if form.is_valid():
-            form.save()
+            student = form.save(commit=False)
+            try:
+                student.username = request.user
+            except Exception:
+                pass
+            student.save()
             return HttpResponseRedirect('/register/?submitted=True')
     else:
         form = StudentForm()
